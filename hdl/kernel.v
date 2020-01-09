b17633bcb5b3ca6a769e5a78298291ea14b5f989
Berin Martini <berin.martini@gmail.com>
2020-01-09 16:55:35 +1100

    Add str_gbox module to kernel_mem
    The str_gbox module will take the kernel weight stream that will be read from
    memory and serialize it to be sent into the kernel memory module.

--- hdl/kernel.v
+++ hdl/kernel.v
@@ -16,6 +16,7 @@
 `define _kernel_
 
 
+`include "str_gbox.v"
 `include "kernel_mem.v"
 
 
@@ -59,12 +60,17 @@ module kernel
      * Internal signals
      */
 
-    reg  [MEM_AWIDTH-1:0]   wr_cfg_end;
-    reg                     wr_cfg_set;
+    reg  [MEM_AWIDTH-1:0]                   wr_cfg_end;
+    reg                                     wr_cfg_set;
+    wire [GROUP_NB*KER_WIDTH*DEPTH_NB-1:0]  wr_data;
+    wire                                    wr_data_val;
+    wire                                    wr_data_rdy;
 
-    reg  [MEM_AWIDTH-1:0]   rd_cfg_start;
-    reg  [MEM_AWIDTH-1:0]   rd_cfg_end;
-    reg                     rd_cfg_set;
+    reg  [MEM_AWIDTH-1:0]                   rd_cfg_start;
+    reg  [MEM_AWIDTH-1:0]                   rd_cfg_end;
+    reg                                     rd_cfg_set;
+    wire [GROUP_NB*KER_WIDTH*DEPTH_NB-1:0]  rd_data;
+    reg                                     rd_data_pop;
 
 
     /**
@@ -95,6 +101,51 @@ module kernel
         end
     end
 
+
+
+    str_gbox #(
+        .DATA_UP_WIDTH  (STR_KER_WIDTH),
+        .DATA_DN_WIDTH  (GROUP_NB*KER_WIDTH*DEPTH_NB))
+    str_gbox_ (
+        .clk        (clk),
+        .rst        (rst),
+
+        .up_data    (str_ker),
+        .up_last    (1'b0),
+        .up_val     (str_ker_val),
+        .up_rdy     (str_ker_rdy),
+
+        .dn_data    (wr_data),
+        .dn_last    (),
+        .dn_val     (wr_data_val),
+        .dn_rdy     (wr_data_rdy)
+    );
+
+
+//    kernel_mem #(
+//        .GROUP_NB   (GROUP_NB),
+//        .KER_WIDTH  (KER_WIDTH),
+//        .DEPTH_NB   (DEPTH_NB),
+//
+//        .MEM_AWIDTH (MEM_AWIDTH),
+//        .MEM_DEPTH  (MEM_DEPTH))
+//    kernel_mem_ (
+//        .clk            (clk),
+//        .rst            (rst),
+//
+//
+//        .wr_cfg_end     (wr_cfg_end),
+//        .wr_cfg_set     (wr_cfg_set),
+//        .wr_data        (wr_data),
+//        .wr_data_val    (wr_data_val),
+//        .wr_data_rdy    (wr_data_rdy),
+//
+//        .rd_cfg_start   (rd_cfg_start),
+//        .rd_cfg_end     (rd_cfg_end),
+//        .rd_cfg_set     (rd_cfg_set),
+//        .rd_data        (rd_data),
+//        .rd_data_pop    (rd_data_pop)
+//    );
 
 
 
