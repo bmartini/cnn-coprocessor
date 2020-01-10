ac52e174b3dc768578d51bba4bc78c26c32cd247
Berin Martini <berin.martini@gmail.com>
2020-01-10 22:31:51 +1100

    Add bias_add module and testbench
    The bias_add module takes the pre-formatted bias value and adds it to the
    incoming stream. The module itself is trivial and might not be worthy of being
    a module but it was done to be consistent with the other operations in the
    layer.

---
+++ hdl/bias_add.v
@@ -0,0 +1,64 @@
+/*
+ * Module:
+ *  bias_add
+ *
+ * Description:
+ *  The bias_add module takes the pre-formatted bias value and adds it to the
+ *  incoming stream.
+ *
+ * Created:
+ *  Fri Jan 10 21:55:56 AEDT 2020
+ *
+ * Author:
+ *  Berin Martini (berin.martini@gmail.com)
+ */
+`ifndef _bias_add_
+`define _bias_add_
+
+
+module bias_add
+  #(parameter
+    NUM_WIDTH = 16)
+   (input                       clk,
+
+    input       [NUM_WIDTH-1:0] bias,
+
+    input       [NUM_WIDTH-1:0] up_data,
+    output reg  [NUM_WIDTH-1:0] dn_data
+);
+
+
+    /**
+     * Local parameters
+     */
+
+
+    function signed [NUM_WIDTH-1:0] addition;
+        input signed [NUM_WIDTH-1:0] a1;
+        input signed [NUM_WIDTH-1:0] a2;
+
+        begin
+            addition = a1 + a2;
+        end
+    endfunction
+
+
+    /**
+     * Internal signals
+     */
+
+
+
+    /**
+     * Implementation
+     */
+
+
+    always @(posedge clk)
+        dn_data <= addition(bias, up_data);
+
+
+
+endmodule
+
+`endif //  `ifndef _bias_add_
