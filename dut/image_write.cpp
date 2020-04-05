d1af9a6c1d26c92315f694cabcc07ad1affee26c
Berin Martini <berin.martini@gmail.com>
2020-04-04 20:48:59 -0700

    Add vcd waveform generation to image_write testbench
    Dump waveform values around each of the clock transitions.

--- dut/image_write.cpp
+++ dut/image_write.cpp
@@ -5,15 +5,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void tick(Vimage_write *dut) {
+void tick(Vimage_write *dut, VerilatedVcdC* wave, vluint64_t timestamp) {
+  printf("%lu\n", (timestamp * 10 - 2));
+
   dut->eval();
+  wave->dump(timestamp * 10 - 2);
+
   dut->clk = 1;
   dut->eval();
+  wave->dump(timestamp * 10);
+
   dut->clk = 0;
   dut->eval();
+  wave->dump(timestamp * 10 + 5);
+  wave->flush();
 }
 
 int main(int argc, char **argv) {
+  vluint64_t timestamp = 0;
 
   // Initialize Verilators variables
   Verilated::commandArgs(argc, argv);
@@ -21,9 +30,18 @@ int main(int argc, char **argv) {
   // Instantiate design
   Vimage_write *dut = new Vimage_write;
 
+  // Generate a trace
+  Verilated::traceEverOn(true);
+  VerilatedVcdC *wave = new VerilatedVcdC;
+  dut->trace(wave, 99);
+  wave->open("image_write.vcd");
+
   for (int x = 0; x < 10; x++) {
-    tick(dut);
+    tick(dut, wave, ++timestamp);
   }
+
+
+  wave->close();
 
   delete dut;
 }
