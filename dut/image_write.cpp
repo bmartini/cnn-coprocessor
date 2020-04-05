51885e77e000fa0a521e4427cf90cc0941a8b8ae
Berin Martini <berin.martini@gmail.com>
2020-04-05 11:27:24 -0700

    Send cfg within image_write cpp testbench
    Mirroring the test case for the Icarus testbench.

--- dut/image_write.cpp
+++ dut/image_write.cpp
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void tick(Vimage_write *dut, VerilatedVcdC* wave, vluint64_t timestamp) {
+void tick(Vimage_write *dut, VerilatedVcdC *wave, vluint64_t timestamp) {
   printf("%lu\n", (timestamp * 10 - 2));
 
   dut->eval();
@@ -21,7 +21,16 @@ void tick(Vimage_write *dut, VerilatedVcdC* wave, vluint64_t timestamp) {
   wave->flush();
 }
 
+void reset(Vimage_write *dut, VerilatedVcdC *wave, vluint64_t timestamp) {
+  dut->rst = 1;
+
+  tick(dut, wave, timestamp);
+
+  dut->rst = 0;
+}
+
 int main(int argc, char **argv) {
+  WData cnt[8] = {0};
   vluint64_t timestamp = 0;
 
   // Initialize Verilators variables
@@ -36,10 +45,52 @@ int main(int argc, char **argv) {
   dut->trace(wave, 99);
   wave->open("image_write.vcd");
 
-  for (int x = 0; x < 10; x++) {
+  reset(dut, wave, ++timestamp);
+
+  tick(dut, wave, ++timestamp);
+
+  dut->cfg_addr = 9; // CFG_IW_IMG_W
+  dut->cfg_data = 0x00000009;
+  dut->cfg_valid = 1;
+  tick(dut, wave, ++timestamp);
+
+  dut->cfg_addr = 10; // CFG_IW_START
+  dut->cfg_data = 0x00000004;
+  dut->cfg_valid = 1;
+  tick(dut, wave, ++timestamp);
+
+  dut->cfg_addr = 11; // CFG_IW_STEP
+  dut->cfg_data = 0x00030009;
+  dut->cfg_valid = 1;
+  tick(dut, wave, ++timestamp);
+
+  dut->cfg_addr = 0;
+  dut->cfg_data = 0;
+  dut->cfg_valid = 0;
+  tick(dut, wave, ++timestamp);
+
+  dut->next = 1;
+  tick(dut, wave, ++timestamp);
+
+  dut->next = 0;
+  tick(dut, wave, ++timestamp);
+
+  for (int x = 0; x < 5; x++) {
     tick(dut, wave, ++timestamp);
   }
 
+  for (int x = 0; x < 5; x++) {
+    cnt[0]++;
+    // dut->str_img_bus = cnt;
+    dut->str_img_val = 1;
+    tick(dut, wave, ++timestamp);
+  }
+
+  printf("size: %lu\n", sizeof(dut->str_img_bus));
+
+  memset(dut->str_img_bus, 0, sizeof(dut->str_img_bus));
+  dut->str_img_val = 0;
+  tick(dut, wave, ++timestamp);
 
   wave->close();
 
